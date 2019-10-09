7a97c121a9ef9c924ae90b2ed85aef45c97cdd89
Anuj Menta <anujmenta@gmail.com>
2019-10-09 18:11:30 -0400

    remove serialize function

--- tests/promotion_factory.py
+++ tests/promotion_factory.py
@@ -12,11 +12,10 @@ class PromotionFactory(factory.Factory):
         model = Promotion
     id = factory.Sequence(lambda n: n)
     code = factory.Faker('promotion_code')
-    percentage = FuzzyChoice(choices=[10, 40, 110, 25, 5, 0, 200, -10])
+    percentage = FuzzyChoice(choices=[10, 40, 30, 25, 5, 0, 15])
     expiry_date = FuzzyChoice(choices=['11-11-2019', '04-01-2018', '01-10-2019', '03-05-2020'])
     start_date = FuzzyChoice(choices=['11-11-2019', '04-01-2018', '01-10-2019', '03-05-2020'])
 
 if __name__ == '__main__':
     for _ in range(10):
-        pet = PromotionFactory()
-        print(pet.serialize())
+        promotion = PromotionFactory()
