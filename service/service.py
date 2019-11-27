2a51f38d8040a0515fd3491e69b276dd8228b03c
Kaili Ding <karryding1121@gmail.com>
2019-11-27 01:50:54 -0500

    refactor the code

--- service/service.py
+++ service/service.py
@@ -152,6 +152,45 @@ class PromotionResource(Resource):
         return '', status.HTTP_204_NO_CONTENT
 
 ######################################################################
+# READ A PROMOTION
+######################################################################
+@app.route('/promotions/<promotion_id>', methods=['GET'])
+def read_a_promotion(promotion_id):
+    """
+    Read a single promotion
+
+    This endpoint will return a Promotion based on it's id
+    """
+    app.logger.info('Read a promotion with id: %s', promotion_id)
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        raise NotFound(
+            "Promotion with id '{}' was not found.".format(promotion_id))
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# UPDATE PROMOTION
+######################################################################
+@app.route('/promotions/<promotion_id>', methods=['PUT'])
+def update_promotions(promotion_id):
+    """
+    Function to update a promotion
+    """
+    app.logger.info(
+        'Request to update promotion with promotion id {}'.format(promotion_id))
+    check_content_type('application/json')
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        raise NotFound(
+            "Promotion with id '{}' was not found.".format(promotion_id))
+    promotion.deserialize(request.get_json())
+    promotion.id = promotion_id
+    promotion.save()
+    app.logger.info(
+        'Promotion with id {} successfully updated'.format(promotion_id))
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+
+######################################################################
 # LIST ALL APIS
 ######################################################################
 @app.route('/apis', methods=['GET'])
@@ -168,64 +207,6 @@ def list_all_apis():
                                  version='1.0',
                                  functions=func_list), status.HTTP_200_OK)
 
-
-######################################################################
-# Error Handlers
-######################################################################
-@app.errorhandler(DataValidationError)
-def request_validation_error(error):
-    """ Handles Value Errors from bad data """
-    return bad_request(error)
-
-
-@app.errorhandler(status.HTTP_400_BAD_REQUEST)
-def bad_request(error):
-    """ Handles bad reuests with 400_BAD_REQUEST """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_400_BAD_REQUEST,
-                   error='Bad Request',
-                   message=message), status.HTTP_400_BAD_REQUEST
-
-
-@app.errorhandler(status.HTTP_404_NOT_FOUND)
-def not_found(error):
-    """ Handles resources not found with 404_NOT_FOUND """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_404_NOT_FOUND,
-                   error='Not Found',
-                   message=message), status.HTTP_404_NOT_FOUND
-
-
-@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
-def method_not_supported(error):
-    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
-                   error='Method not Allowed',
-                   message=message), status.HTTP_405_METHOD_NOT_ALLOWED
-
-
-@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-def mediatype_not_supported(error):
-    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
-    message = str(error)
-    app.logger.warning(message)
-    return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-                   error='Unsupported media type',
-                   message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
-
-
-@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
-def internal_server_error(error):
-    """ Handles unexpected server error with 500_SERVER_ERROR """
-    message = str(error)
-    app.logger.error(message)
-    return jsonify(status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                   error='Internal Server Error',
-                   message=message), status.HTTP_500_INTERNAL_SERVER_ERROR
 
 ######################################################################
 # Apply a promotion on products
@@ -285,43 +266,53 @@ def apply_a_promotion(promotion_id):
 
 
 ######################################################################
-# READ A PROMOTION
+# Error Handlers
 ######################################################################
-@app.route('/promotions/<promotion_id>', methods=['GET'])
-def read_a_promotion(promotion_id):
-    """
-    Read a single promotion
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
 
-    This endpoint will return a Promotion based on it's id
-    """
-    app.logger.info('Read a promotion with id: %s', promotion_id)
-    promotion = Promotion.find(promotion_id)
-    if not promotion:
-        raise NotFound(
-            "Promotion with id '{}' was not found.".format(promotion_id))
-    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
 
-######################################################################
-# UPDATE PROMOTION
-######################################################################
-@app.route('/promotions/<promotion_id>', methods=['PUT'])
-def update_promotions(promotion_id):
-    """
-    Function to update a promotion
-    """
-    app.logger.info(
-        'Request to update promotion with promotion id {}'.format(promotion_id))
-    check_content_type('application/json')
-    promotion = Promotion.find(promotion_id)
-    if not promotion:
-        raise NotFound(
-            "Promotion with id '{}' was not found.".format(promotion_id))
-    promotion.deserialize(request.get_json())
-    promotion.id = promotion_id
-    promotion.save()
-    app.logger.info(
-        'Promotion with id {} successfully updated'.format(promotion_id))
-    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_400_BAD_REQUEST,
+                   error='Bad Request',
+                   message=message), status.HTTP_400_BAD_REQUEST
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_404_NOT_FOUND,
+                   error='Not Found',
+                   message=message), status.HTTP_404_NOT_FOUND
+
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                   error='Method not Allowed',
+                   message=message), status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                   error='Unsupported media type',
+                   message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
